6b431813aa9f57cbda40dde7c332e03b22954737
pamusa ngom <pngom1@student.gsu.edu>
2026-02-13 01:29:22 -0500

    Arranging data into a table

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -16,19 +16,38 @@ for match in data["matches"]:
         home_goals = score["ft"][0]
         away_goals = score["ft"][1] 
         played = True
+
+        if home_goals > away_goals:
+            result = "H"
+        elif home_goals < away_goals: 
+            result = "A"
+        else:
+            result = "D"
     
     else:
         home_goals = None
         away_goals = None
         played = False
+        result = None
 
     matches.append({
         "home_team": home,
         "away_team": away,
         "home_goals": home_goals,
         "away_goals": away_goals,
+        "result": result,
         "played": played
     })
+
+played_matches = [m for m in matches if m["played"]]
+upcoming_matches = [m for m in matches if not m["played"]]
+
+import pandas as pd
+
+df = pd.DataFrame(played_matches)
+
+print(df.head())
+print("\nColumns:", df.columns)
 
 for m in matches[:5]:
     print(m)
